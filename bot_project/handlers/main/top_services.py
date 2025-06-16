ecf0a8f8bbb3b510261e8e17d6e8bcdc8f6a931b
FlashTheFire <harshtakur001@gmail.com>
2025-06-17 01:31:29 +0530

    Update: .env and other changes

--- bot_project/handlers/main/top_services.py
+++ bot_project/handlers/main/top_services.py
@@ -425,9 +425,9 @@ class TopServiceManager:
                     else:
                         pass
                 await asyncio.sleep(self.UPDATE_INTERVAL)
-            except asyncio.CancelledError:
-                print("Automatic leaderboard update task cancelled")
-                await asyncio.sleep(60)
+            except asyncio.CancelledError as e:
+                print(f"Automatic leaderboard update task cancelled : {e}")
+                raise f"Automatic leaderboard update task cancelled : {e}"
             except Exception as e:
                 print(f"Error in automatic leaderboard update: {e}\n{traceback.format_exc()}")
                 await asyncio.sleep(60)
