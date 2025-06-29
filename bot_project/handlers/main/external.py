92e86f62dc5b925037de61dd71ed39e7ffce1b01
FlashTheFire <harshtakur001@gmail.com>
2025-06-29 08:29:48 +0530

    Update: .env and other changes

--- bot_project/handlers/main/external.py
+++ bot_project/handlers/main/external.py
@@ -311,8 +311,8 @@ class ForwardManager:
                 elif action == self.CB_REMOVE_COUNTRY:
                     await self._update_list(chat_id, text, self.country_list, "Country", False)
                 elif action == self.CB_CHECK_NUM:
-                    gtext = self.to_gtext(message.text)
-                    text = gtext
+                    #gtext = self.to_gtext(message.text)
+                    #text = gtext
                     all_numbers = [
                         num.strip() 
                         for num in text.splitlines() 
